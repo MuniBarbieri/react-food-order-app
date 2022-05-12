3b2e3e906df00581d72044e6f25f921380aff01b
MuniBarbieri <edmundobarbieri@gmail.com>
2022-05-12 18:20:23 +1000

    cartProvider: Improve reducer in order to show only one li in the cart if the item is repeated

--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -11,10 +11,30 @@ const CartProvider = props => {
   const cartReducer = (state, action) => {
     switch (action.type) {
       case "ADD_ITEM":
-        const updatedTotalAmount = state.totalAmount + action.payload.price * action.payload.amount
+        const updatedTotalAmount = state.totalAmount + action.payload.price * action.payload.amount 
+
+        const indexOfRepeatedItem = state.items.findIndex(item => {
+          return item.id === action.payload.id
+        })
+
+        const existingCartItem = state.items[indexOfRepeatedItem]
+
+        let updatedItems
+
+        if (existingCartItem) {
+           const updatedItem = {
+            ...existingCartItem,
+            amount: existingCartItem.amount + action.payload.amount
+          }
+          updatedItems = [...state.items]
+          updatedItems[indexOfRepeatedItem] = updatedItem
+        } else {
+          updatedItems = [...state.items, action.payload]
+        }
+        
         return {
           ...state,
-          items: [...state.items, action.payload],
+          items: updatedItems,
           totalAmount: updatedTotalAmount
         }
       case "REMOVE_ITEM":
