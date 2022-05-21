ad4adfea699a2d497233f09e66590372a9ea533d
MuniBarbieri <edmundobarbieri@gmail.com>
2022-05-22 06:53:39 +1000

    Meals moved to firebase real time data base Meals fetched from the BE

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,14 @@
-import classes from "./AvailableMeals.module.css"
+import { useEffect, useState } from "react";
 import Card from "../UI/Card"
 import MealItem from "./MealItem/MealItem"
+import classes from "./AvailableMeals.module.css"
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
 
 const AvailableMeals = () => {
 
-  const mealList = DUMMY_MEALS.map(meal => <MealItem
+  const [ meals, setMeals] = useState([])
+
+  const mealList = meals.map(meal => <MealItem
     id={meal.id}
     key={meal.id}
     name={meal.name}
@@ -39,6 +16,24 @@ const AvailableMeals = () => {
     price={meal.price} />
   )
 
+  useEffect(() => {
+    const fetchMeals = async () => { 
+      const response = await fetch('https://react-food-app-ab32f-default-rtdb.firebaseio.com/meals.json')
+      const responseData = await response.json()
+      const responseDataEntries = Object.entries(responseData)
+
+      const loadedMeals = responseDataEntries.map(el => {
+        const meals = {
+          id: el[0],
+          ...el[1]
+        }
+        return meals
+      })
+      setMeals(loadedMeals)
+    }
+    fetchMeals()
+  }, [])
+  
   return (
     <section className={classes.meals}>
       <Card>
