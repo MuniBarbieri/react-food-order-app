b586187bca86bd268d16926232cb6d4f06d699b5
MuniBarbieri <edmundobarbieri@gmail.com>
2022-05-23 09:11:33 +1000

    AvailableMeals: Added loading to improve ux Added error handling to improve ux

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,7 +6,9 @@ import classes from "./AvailableMeals.module.css"
 
 const AvailableMeals = () => {
 
-  const [ meals, setMeals] = useState([])
+  const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [ httpError, setHttpError] = useState({error:false,errorMessage:''})
 
   const mealList = meals.map(meal => <MealItem
     id={meal.id}
@@ -19,6 +21,11 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => { 
       const response = await fetch('https://react-food-app-ab32f-default-rtdb.firebaseio.com/meals.json')
+      
+      if (!response.ok) {
+        throw Error ('Something went wrong!')
+      }
+
       const responseData = await response.json()
       const responseDataEntries = Object.entries(responseData)
 
@@ -30,14 +37,22 @@ const AvailableMeals = () => {
         return meals
       })
       setMeals(loadedMeals)
+      setIsLoading(false)
     }
-    fetchMeals()
-  }, [])
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError({error:true,errorMessage:error.message})
+    })
+  
+  }, [isLoading,httpError.error])
   
   return (
     <section className={classes.meals}>
       <Card>
         <ul>
+          {httpError.error &&
+            <p className={classes.mealsError}> Failed to fetch </p>}
+          {isLoading && <p className={classes.mealsLoading} >Loading meals....</p>}
         {mealList}
         </ul>
       </Card>
